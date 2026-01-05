3925cd84d50dfeb9ff7363add527116d4addbc8f
valereoo <valereo3201@gmail.com>
2026-01-05 17:34:49 +0700

    Minor Fix

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use('/api/drinks', drinkRoutes);
 app.use('/api/auth', authRoutes);
 
-app.get('/', (req, res) => res.send('Server Reo Jalan!'));
+app.get('/', (req, res) => res.send('Server Jalan'));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
